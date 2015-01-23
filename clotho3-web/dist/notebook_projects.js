049932f6acfd16b2aa5dab5c6a96ca39ef503895
Katie Lewis <kmlewis@bu.edu>
2015-01-23 17:10:25 -0500

    Unknown changes

---
+++ clotho3-web/dist/notebook_projects.js
@@ -0,0 +1,20 @@
+<SCRIPT LANGUAGE = "javascript">
+
+<!--
+
+function notebook_projects(dropdown)
+{
+	var myIndex = dropdown.selectedIndex
+	var SelValue = dropdown.options[myindex].SelValue
+	if (SelValue == "Kathleen Lewis")
+	{
+		document.getElementById('notebookID').disabled = false;
+	}
+	else
+	{
+		document.getElementById('notebookID').disabled = true;
+	}
+}
+
+//-->
+</SCRIPT>
