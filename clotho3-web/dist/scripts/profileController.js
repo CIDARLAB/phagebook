9e54447194d374ab518258a49c9a30d2c07160ee
Allison Durkan <azulad7@bu.edu>
2015-07-31 17:03:40 -0400

    stores pubmed ids in array in the person object and uses a popup window to query for colleagues

--- clotho3-web/dist/scripts/profileController.js
+++ clotho3-web/dist/scripts/profileController.js
@@ -1,6 +1,5 @@
-//angular.module('pubmedRoot',[]).run(["pubmed",function(){}]);
-angular.module('profileApp',['clothoRoot'/*,'pubmedRoot'*/]).controller('profileController',
-function($scope, Clotho/*, pubmed*/){
+angular.module('profileApp',['clothoRoot','ui.bootstrap.modal']).controller('profileController',
+function($scope, Clotho, $modal){
     $scope.personObj = {};
     $scope.personID = sessionStorage.getItem("uniqueid");
 
@@ -11,16 +10,6 @@ function($scope, Clotho/*, pubmed*/){
             $scope.displayName = person.fullname;
             $scope.pictureName = person.givenname + person.surname;
             $scope.$apply();
-
-            if (person.pubmedId == "") {
-                $scope.idExist = false;
-            }
-            else {
-                $scope.idExist = true;
-                /*pubmed.getCitationsFromIds(person.pubmedId).then(function(result){
-                    $scope.publications = result;
-                });*/
-            }
 
         });
     });
@@ -47,52 +36,52 @@ function($scope, Clotho/*, pubmed*/){
 
     $scope.createStatus = function() {
         $scope.timeStamp = new Date();
-        //$scope.statusObj = "{ \"schema\": \"org.clothocad.phagebook.status\", \"time\" :\"" + $scope.timeStamp + "\" , \"text\": \"" + $scope.newStatus + "\" , \"person\": \"" + $scope.personID + "\" }";
         $scope.statusObj = {
             "statusMessaage" : $scope.newStatus,
             "timeStamp" : $scope.timeStamp
             //eventually there could be a location key value pair
         };
-        //$scope.obj = JSON.parse($scope.statusObj);
+
         Clotho.create($scope.statusObj).then(function() {
             Clotho.get($scope.personID).then(function () {
                 $scope.personObj['statusList'].push($scope.statusObj);
                 Clotho.set($scope.personObj);
-                //console.log(JSON.stringify($scope.personObj));
             });
         });
+        $scope.statuses = $scope.personObj['statusList'];
     };
 
     $scope.loadFriends = function() {
-        if ($scope.personObj.friendsList == null){
 
-        }
-        else {
-
-        }
-
-    };
-
-    $scope.loadStatuses = function(){
-        console.log("WIEGHIWEGJO")
-        for ($scope.i = 0; i < 3; i++){
-            console.log(i);
-        }
-
-      //$scope.loadedStatuses = $scope.personObj.statusList[4]['statusMessage'];
     };
 
     $scope.displayPub = function() {
 
         Clotho.get($scope.personID).then(function(){
-            $scope.personObj['pubmedId'] = $scope.pubmedId;
+            $scope.personObj['pubmedIdList'].push($scope.pubmedId);
             console.log(JSON.stringify($scope.personObj));
             Clotho.set($scope.personObj);
         });
 
-        $scope.idExist = true;
+        $scope.publications = pubmed.getCitationsFromIds($scope.personObj['pubmedIdList']);
     };
 
+    $scope.findFriends = function(size) {
+            var myFriendSearch = $modal.open({
+                templateURL: 'friendFinder.html',
+                controller: 'profileWindowController',
+                size: size,
+                resolve: {
+                    items: function () {
+                        //this object will get passed to the modal's controlller
+                        return {myPerson: {name: "Bobby"}, myData: 42};
+                    }
+                }
+            });
+            myFriendSearch.result.then(function (items) {
+                //do stuff with returned data, like Clotho.set??
+            });
+        };
 });
 
 
---
+++ clotho3-web/dist/scripts/profileWindowController.js
@@ -0,0 +1,14 @@
+angular.module('profileApp',['clothoRoot','ui.bootstrap.modal']).controller('profileWindowController',
+function($scope, $modalInstance, items){
+   $scope.ok = function() {
+       $modalInstance.close(items);
+   };
+
+    $scope.cancel = function() {
+      $modalInstance.dismiss('cancel');
+    };
+
+    $scope.someOtherFunction = function(){
+      //other stuff here
+    };
+});
