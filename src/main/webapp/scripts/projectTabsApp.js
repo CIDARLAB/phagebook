1694d9ccedad6624c45f89d92758a2d9e23ff352
Allison Durkan <azulad7@bu.edu>
2015-12-07 15:40:03 -0500

    implemented the directives in all of the pages I think, sidebar has new and improved navigation

--- src/main/webapp/scripts/projectTabsApp.js
+++
@@ -1,7 +0,0 @@
-angular
-        .module('tabsApp', ['clothoRoot'])
-        .controller('tabsController', tabsController)
-        .controller('projectsController', projectsController)
-        .directive('phagebookSidebar', phagebookSidebar)
-        .directive('searchBar', searchBar)
-        .directive('footer',footer); 
--- src/main/webapp/scripts/projectsApp.js
+++ src/main/webapp/scripts/projectsApp.js
@@ -1,5 +1,5 @@
 angular
-        .module('projectsApp', ['clothoRoot','ui.bootstrap'])
+        .module('projectsApp', ['ui.bootstrap'])
         .controller('tabsController', tabsController)
         .controller('projectsController', projectsController)
         .directive('phagebookSidebar', phagebookSidebar)
