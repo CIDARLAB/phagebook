7e981c81135e011d5d0b72b7b2ef37ce8cd5620d
Johan Ospina <Johanos@bu.edu>
2016-01-23 13:23:37 -0500

    Revert "stuff"
    This reverts commit 627734369a7d59f196cbfe2b41cfbe8885e96d9f.

--- src/main/webapp/scripts/orderingApp.js
+++
@@ -1,7 +0,0 @@
-angular
-        .module('orderingApp', ['ui.bootstrap'])
-        .controller('orderingCtrl', orderingCtrl)
-        .controller('newOrderController', newOrderController)
-        .controller('orderingSettingsController', orderingSettingsController)
-        .directive('phagebookSidebar', phagebookSidebar)
-        .directive('footer', footer); 
