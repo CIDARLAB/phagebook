8dd8829d5c80c70fc517af2abf6505a5829a71c0
Johan Ospina <Johanos@bu.edu>
2016-02-12 12:40:45 -0500

    THE YEAR IS 20XX and we have COOKIEHANDLER.JS. featuring your favorite functions such as add cookie, delete cookie, append to cookie, update cookie... also there was refactoring of Javascript to make our lives easier, put functionality to put a cookie with the logged in user's id that can be gotten anywhere we want

--- src/main/webapp/scripts/ordering.js
+++ src/main/webapp/scripts/ordering.js
@@ -70,7 +70,8 @@ function orderingCtrl($scope) {
 function createOrder() {
     if (getCookie("Order") !== "") {
         var ids = getCookie("Order").split(',');
-       
+        var userId = getCookie("loggedInUserId");
+        /*TODO NOW WE CAN KNOW WHO's LOGGED IN SO WE CAN WORK WITH THAT*/
         var name = $("#orderNameBox").val();
         var description = $("#orderDescriptionBox").val();
         var idQuantityCombinations = {};
@@ -166,53 +167,3 @@ function totalNumberOfBoxesWithName(name) {
     return productNumber;
 }
 
-
-
-
-function setCookie(cname, cvalue, exdays)
-{
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + "; " + expires;
-}
-function getCookie(cname) {
-    //GET A DAMN COOKIE VALUE
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ')
-            c = c.substring(1);
-        if (c.indexOf(name) == 0)
-            return c.substring(name.length, c.length);
-    }
-    return "";
-    //
-}
-function appendToCookie(cname, cvalue, exdays) {
-
-
-    var currentCookie = getCookie(cname);
-
-
-    var updatedCookie = "";
-    if (currentCookie == "") {
-        updatedCookie = cvalue;
-    } else if (currentCookie != "") {
-        updatedCookie = currentCookie + ',' + cvalue;
-    }
-    setCookie("Order", updatedCookie, exdays);
-}
-
-function delete_cookie(name) {
-    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-}
-
-function getParameterByName(name)
-{
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-            results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
