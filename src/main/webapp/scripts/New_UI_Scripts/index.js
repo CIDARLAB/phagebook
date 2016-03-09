3c015a882983297e15a2e462520a82970786abfa
Johan Ospina <Johanos@bu.edu>
2016-03-09 11:40:07 -0500

    adding external javascript

--- src/main/webapp/scripts/New_UI_Scripts/index.js
+++ src/main/webapp/scripts/New_UI_Scripts/index.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
-    $( "#createProfile" ).click( function() {
-         alert( "Handler for .click() called." );
-    });
-
+    
 });
+
+function checkPasswordMatch() {
+    var password = $("#password").val();
+    var confirmPassword = $("#reenterPassword").val();
+
+    if (password !== confirmPassword)
+       console.log("Passwords do not match");
+    else
+       console.log("Passwords match");
+}
