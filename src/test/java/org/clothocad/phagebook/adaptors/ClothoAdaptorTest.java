39b2efde675cd50ba0ccdf334af41147d34b18e6
Johan Ospina <Johanos@bu.edu>
2016-03-10 10:54:14 -0500

    mergin

---
+++ src/test/java/org/clothocad/phagebook/adaptors/ClothoAdaptorTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.clothocad.phagebook.adaptors;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.clothoapi.clotho3javaapi.Clotho;
+import org.clothoapi.clotho3javaapi.ClothoConnection;
+import org.clothocad.phagebook.controller.Args;
+import org.clothocad.model.Person;
+import org.clothocad.phagebook.security.EmailSaltHasher;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Herb
+ */
+public class ClothoAdaptorTest {
+    
+    public ClothoAdaptorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of createProduct method, of class ClothoAdaptor.
+     */
+    @Test
+    public void testCreateProduct() {
+    }
+}
