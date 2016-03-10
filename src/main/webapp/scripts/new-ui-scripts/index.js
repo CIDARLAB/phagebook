96a405795f7da2edd29f57639526de4054dc14a3
Johan Ospina <Johanos@bu.edu>
2016-03-10 11:16:37 -0500

    merging

--- src/main/webapp/scripts/new-ui-scripts/index.js
+++ src/main/webapp/scripts/new-ui-scripts/index.js
@@ -1,5 +1,33 @@
 $(document).ready(function() {
-    
+    $('#createProfile').click( function () {
+        
+        var isValid = 1;
+        
+        if (isValid){
+         $.ajax({
+                    url: "createPerson",
+                    type: "POST",
+                    async: false,
+                    data: {
+                        //"id": loginResult.id,
+                        "firstName": document.getElementById("inputFirstName").value,
+                        "lastName": document.getElementById("inputLastName").value,
+                        "emailId":  document.getElementById("emailAddress").value,
+                        "password": document.getElementById("password").value
+                    },
+                    success: function (response) {
+                        
+                        
+
+                        window.location.href = './html/validateEmail.html';
+                    },
+                    error: function (response) {
+                        //THIS CAN BE DONE BETTER ONCE WE KNOW WHAT WE ARE DOING.
+                        alert("\n" +  response.statusText + "!\n" + response.responseText);
+                    }
+                });
+        }
+    })
 });
 
 function checkPasswordMatch() {
