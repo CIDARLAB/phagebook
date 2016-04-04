5f14f5632fcb6576e7b3f9160a83a770479bd899
Johan Ospina <Johanos@bu.edu>
2016-04-04 14:56:45 -0400

    Stable version

--- src/main/webapp/scripts/new-ui-scripts/accountSettings.js
+++ src/main/webapp/scripts/new-ui-scripts/accountSettings.js
@@ -19,13 +19,18 @@ function documentReady(){
             removeOptions(select);
             var length = response.institutions.length; //has institutions and labs
             var count = 0;
+            var opt = document.createElement('option');
+            opt.value ="";
+            opt.innerHTML = "...";
+            select.appendChild(opt);
             for (var i = 0; i < length; i++){
 
-                var opt = document.createElement('option');
+                opt = document.createElement('option');
                 opt.value = response.institutions[i].institutitonId;
 
                 opt.innerHTML = response.institutions[i].institutionName;
                 select.appendChild(opt);
+
 
             }
         },
@@ -34,14 +39,15 @@ function documentReady(){
         }
     });
 
-    $('#institution').change( function () {
+    $('#institution-drop-down').change( function () {
+
 
         var selectedInstitution =  $('#institution-drop-down option').filter(':selected').text();
 
         var responseArray = JSON.parse(sessionStorage.getItem("index-institutions"));
 
 
-        var selectLabs = document.getElementById('lab-name');
+        var selectLabs = document.getElementById('pi-lab-results');
 
 
         var numberOfInstitutions = responseArray.length;
@@ -56,14 +62,15 @@ function documentReady(){
                 for (var j = 0 ; j < labsLength ; j++){
                     var opt2 = document.createElement('option');
                     opt2.value = labsArray[j].labId;
+
                     opt2.innerHTML = labsArray[j].labName;
                     selectLabs.appendChild(opt2);
                 }
-            } else if ( selectedInstitution === "Institution..."){
+            } else if ( selectedInstitution === "..."){
                 removeOptions(selectLabs);
                 var opt2 = document.createElement('option');
                 opt2.value = "";
-                opt2.innerHTML = "Lab Name...";
+                opt2.innerHTML = "...";
                 selectLabs.appendChild(opt2);
                 return;
             }
@@ -71,6 +78,47 @@ function documentReady(){
 
         }
     });
+
+    $('#load-pis').click(function (){
+        var selectedLab = $('#pi-lab-results option').filter(':selected')[0].value;
+        if (selectedLab != "..."){
+            doLabAjaxCall(selectedLab);
+        }
+
+    })
 }
 
+
+function removeOptions(selectbox)
+{
+    var i;
+
+    for( i = selectbox.options.length-1 ; i>=0 ; i--)
+    {
+        selectbox.remove(i);
+    }
+}
+
+function doLabAjaxCall(labId){
+    $.ajax({
+        //do this for projects...
+
+        url: "../listPIsOfLab",
+        type: "GET",
+        async: false,
+        data: {
+                "lab":labId
+        },
+        success: function (response) {
+
+            for (var i =0; i  <  response.length; i++){
+               alert("PI" + i + ": " + response[i].name + " Email: " + response[i].email + " ClothoId: " + response[i].clothoId );
+            }
+
+        },
+        error: function (response) {
+
+        }
+    });
+}
 
