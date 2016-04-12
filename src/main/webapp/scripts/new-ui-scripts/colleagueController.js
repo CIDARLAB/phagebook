d67fe1ce88c4b1cde4eea9b312354dba1e406436
Allison Durkan <azulad7@bu.edu>
2016-04-12 01:44:50 -0400

    we are literal rockstars

--- src/main/webapp/scripts/new-ui-scripts/colleagueController.js
+++ src/main/webapp/scripts/new-ui-scripts/colleagueController.js
@@ -9,79 +9,108 @@ function colleagueCtrl($scope, $http) {
     $scope.profilePictureLink = awsPath + fileExt;
 
     angular.element(document).ready(function ($scope) {
-        console.log("before ajax but inside page onload.`");
-        $.ajax({
-            type: 'GET',
-            url: '../getPersonById',
-            data: {
-                "userId": clothoId
-            },
-            success: function (response) {
-                var responseAsJSON = angular.fromJson(response);
-                console.log(JSON.stringify(responseAsJSON));
-                $("#profile-name").text(responseAsJSON.fullname);
-                $("#profile-department").text(responseAsJSON.department);
-                $("#profile-institution").text(responseAsJSON.institution);
-                $("#profile-title").text(responseAsJSON.title);
-                $("#profile-description").text(responseAsJSON.profileDescription);
-                document.getElementById('add-colleague-btn').value = clothoId;
-                //$scope.statuses = responseAsJSON.statuses;
-            },
-            error: {
-                //console.log("inside GET error");
-            } 
-        });
 
         $("#add-colleague-btn").click(function () {
-            console.log(this.value);  
+            console.log(this.value);
             $.ajax({
                 type: 'POST',
                 url: '../addColleagueRequest',
                 data: {
-                    "colleagueClothoId" : this.value,
-                    "loggedInClothoId" : getCookie("clothoId")
-                },
-                success: function(response) {
-                    
-                },
-                error: function(response) {
-                    
-                }
-            });
-
-        }); 
-        
-        
-        $("#load-more-btn").click(function () {
-            $.ajax({
-                type: 'GET',
-                url: '../loadUserStatuses',
-                data: {
-                    "clothoId": clothoId
+                    "colleagueClothoId": this.value,
+                    "loggedInClothoId": getCookie("clothoId")
                 },
                 success: function (response) {
-                    var responseAsJSON = angular.fromJson(response);
-                    console.log(JSON.stringify(responseAsJSON));
 
-                    var ul = $("#status-list");
-                    ul.empty();
-                    for (var i = response.length - 1; i >= 0; i--) {
-                        var tmpl = document.getElementById("status-template").content.cloneNode(true);
-                        var now = new Date(response[i].dateCreated);
-                        tmpl.querySelector(".status-date").innerText = "Created On: " + response[i].dateCreated;
-                        tmpl.querySelector(".status-text").innerText = response[i].statusText;
-                        ul.append(tmpl);
-                    }
                 },
-                error: {
-                    //console.log("inside GET error");
+                error: function (response) {
+
                 }
             });
+
         });
+
+        $.ajax({
+            type: 'GET',
+            url: '../loadColleagues',
+            async: false,
+            data: {
+                "userId": clothoId
+            },
+            success: function (response) {
+                var ul = $("#colleagues-full-list");
+                ul.empty();
+                console.log(JSON.stringify(response));
+                for (var i = 0; i < response.length; i++) {
+                    var tmpl = document.getElementById("colleague-display-template").content.cloneNode(true);
+                    // var othertmpl = document.getElementById("colleague-page-template").content.cloneNode(true);
+                    tmpl.querySelector(".colleague-page-picLink").src = "http://s3.amazonaws.com/phagebookaws/" + response[i].clothoId + "/profilePicture.jpg";
+                    tmpl.querySelector(".colleague-page-picLink").alt = response[i].fullname;
+                    tmpl.querySelector(".colleague-display-fullname").text = response[i].fullname;
+                    tmpl.querySelector(".colleague-display-lab").innerHTML = (response[i].labName == null) ? "" : response[i].labName;
+                    tmpl.querySelector(".colleague-display-institution").innerHTML = response[i].institutionName;
+                    tmpl.querySelector(".colleague-display-fullname").href = "../html/colleague.html?user=" + response[i].clothoId;
+                    ul.append(tmpl);
+                }
+            },
+            error: {
+                //console.log("inside GET error");
+            }
+        });
+
+        $.ajax({
+            type: 'GET',
+            url: '../loadUserStatuses',
+            async: false,
+            data: {
+                "clothoId": clothoId
+            },
+            success: function (response) {
+                var responseAsJSON = angular.fromJson(response);
+                console.log(JSON.stringify(responseAsJSON));
+
+                var ul = $("#status-list");
+                ul.empty();
+                for (var i = response.length - 1; i >= 0; i--) {
+                    var tmpl = document.getElementById("status-template").content.cloneNode(true);
+                    var now = new Date(response[i].dateCreated);
+                    tmpl.querySelector(".status-date").innerText = "Created On: " + response[i].dateCreated;
+                    tmpl.querySelector(".status-text").innerText = response[i].statusText;
+                    ul.append(tmpl);
+                }
+            },
+            error: {
+                //console.log("inside GET error");
+            }
+        });
+
     });
+
+$(document).ready(function () {
+    var clothoId = getParameterByName("user");
+    console.log("before ajax but inside page onload.`");
+    $.ajax({
+        type: 'GET',
+        url: '../getPersonById',
+        data: {
+            "userId": clothoId
+        },
+        success: function (response) {
+            var responseAsJSON = angular.fromJson(response);
+            console.log(JSON.stringify(responseAsJSON));
+            $("#profile-name").text(responseAsJSON.fullname);
+            $("#profile-dept").text(responseAsJSON.department);
+            $("#profile-institution").text(responseAsJSON.institution);
+            $("#profile-title").text(responseAsJSON.title);
+            $("#profile-description").text(responseAsJSON.profileDescription);
+            document.getElementById('add-colleague-btn').value = clothoId;
+            //$scope.statuses = responseAsJSON.statuses;
+        },
+        error: {
+            //console.log("inside GET error");
+        }
+    });
+});
 }
-
-
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
