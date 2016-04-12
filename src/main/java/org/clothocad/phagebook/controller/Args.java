091b431183dcbdf8e7dafa5d3fd5a9d15971c553
PrashantVaidyanathan <prash@bu.edu>
2016-04-12 03:47:53 -0400

    changed phagebook location

--- src/main/java/org/clothocad/phagebook/controller/Args.java
+++ src/main/java/org/clothocad/phagebook/controller/Args.java
@@ -12,7 +12,7 @@ package org.clothocad.phagebook.controller;
 public class Args {
     
     public static final String clothoLocation = "wss://clothocad.org:8443/websocket";
-    public static final String phagebookBaseURL = "https://phagebook.cidarlab.org:9090";
+    public static final String phagebookBaseURL = "http://cidarlab.org:9090";
     public static final String defaultPhagebookUsername = "phagebook";
     public static final String defaultPhagebookPassword = "backend";
     
