52b4c099dbe9792135ccb20b2907d7316ac1be8a
agoncharova <agonchar@bu.edu>
2016-07-17 14:55:59 -0400

    add new email code

---
+++ src/main/java/org/clothocad/phagebook/adaptors/EmailCredentials.java
@@ -0,0 +1,87 @@
+package org.clothocad.phagebook.adaptors;
+
+import java.util.ArrayList;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import static org.clothocad.phagebook.adaptors.EmailHandler.SENDER_DOMAIN_NAME;
+import static org.clothocad.phagebook.adaptors.EmailHandler.SENDER_PASSWORD;
+
+/**
+ *
+ * @author anna_g
+ * 
+ * This class is the hub for all emailing operations
+ * 
+ */
+public class EmailCredentials {
+    public final static String SENDER_DOMAIN_NAME = "phagebook.email@gmail.com";
+    public final static String SENDER_PASSWORD = "Phagebook";
+    
+    private static EmailCredentials instance = null;
+    protected EmailCredentials(){
+        
+    }
+    public static EmailCredentials getEmailCredentials(){
+        if (instance == null){
+            instance = new EmailCredentials();
+        }
+        return instance; 
+    }
+    
+    /**
+    * @param messageBody Pass in the HTML version of the message body
+    * @param messageSubject Pass in the subject for this email
+    * 
+    * Create a session object with the proper email credentials.
+    * 
+    * Return a boolean to signify success.
+    * 
+    */
+    public static boolean logInAndSendMessage(String messageBody, String messageSubject, 
+            ArrayList<String> sendTo){
+      Properties props = new Properties();
+
+      props.put("mail.smtp.host", "smtp.gmail.com");
+      props.put("mail.smtp.socketFactory.port", "465");
+      props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+      props.put("mail.smtp.auth", "true");
+      props.put("mail.smtp.port", "465");
+      props.put("mail.smtp.starttls.enable", "true");
+      
+      // create a session object with information about Phagebook email address
+      Session session = Session.getInstance(props,new javax.mail.Authenticator() {            
+        protected PasswordAuthentication getPasswordAuthentication() {
+          return new PasswordAuthentication(SENDER_DOMAIN_NAME, SENDER_PASSWORD);
+        }
+      });
+      
+      MimeMessage message;
+      try{
+        // create a message object with default properties
+        message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(SENDER_DOMAIN_NAME));
+        message.setSubject(messageSubject);
+        message.setContent(messageBody,"text/html");
+        
+        for(int i = 0;i<sendTo.size();i++){
+          String recepient = sendTo.get(i);
+          message.addRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
+          // Send message
+          Transport.send(message);
+        }
+
+        return true;
+      }catch (MessagingException mex) {
+        mex.printStackTrace();
+        return false;
+      }
+    
+    }
+  
+}
---
+++ src/test/java/org/clothocad/phagebook/adaptors/servlets/EmailCredsTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.clothocad.phagebook.adaptors.servlets;
+
+import java.util.ArrayList;
+import org.clothocad.phagebook.adaptors.EmailCredentials;
+import org.junit.Test;
+
+/**
+ *
+ * @author anna_g
+ */
+public class EmailCredsTest {
+  
+   @Test
+   public void testEmailStuff() throws Exception {
+     ArrayList<String> emailAdr = new ArrayList();
+     emailAdr.add("agonchar@bu.edu");
+     emailAdr.add("anna@goncharova.com");
+     emailAdr.add("anna.goncharova00@gmail.com");
+     
+      String subject = "Testing the new emailing class";
+      String body = "<i>Testing HTML properties</i><br><b>This should be bold!</b>";      
+      EmailCredentials.logInAndSendMessage(body,subject, emailAdr);     
+     
+   }
+  
+  
+}
